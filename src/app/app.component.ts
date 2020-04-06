3d779648701c5ba010c269cab4cf88859f6f4a7e
shaozhun <712104878@qq.com>
2020-04-06 16:47:26 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
+const todos = [{
+  id: 1,
+  title: '吃饭',
+  done: true
+}, {
+  id: 2,
+  title: '睡觉',
+  done: true
+}, {
+  id: 3,
+  title: '写代码',
+  done: true
+}]
+// 在angular中，组件就是一个类
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: '',
   styleUrls: ['./app.component.css']
 })
+
+// 在vue中，组件有自己的data和methods等选项
 export class AppComponent {
-  title = 'angular-pc';
+  public todos: {
+    id: number,
+    title: string,
+    done: boolean
+  }[] = todos;
+  addTodo(e): void {
+    const titleText = e.target.value
+    if (!titleText.length) {
+      return
+    }
+    const last = this.todos[this.todos.length - 1]
+    this.todos.push({
+      id: last ? last.id + 1 : 1,
+      title: titleText,
+      done: false
+    })
+    e.target.value = ''  //清除文本框
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooComponent } from './foo/foo.component';
 
 @NgModule({
+  // 组装模块资源： 组件，指令，服务
   declarations: [
-    AppComponent
+    AppComponent,
+    FooComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //指定启动组件，指定哪个模块为入口
 })
 export class AppModule { }
